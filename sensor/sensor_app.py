b99f7099f27119ce4111ac385d65ddc61b5001f1
pedro.dg <pedro.goncalves.arch@gmail.com>
2022-10-03 12:15:41 +0100

    module1 tasks

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,6 @@
 # Runner script for all modules
 
+from load_data import load_sensor_data
 
 ##############################
 # Do not remove these two lines
@@ -10,6 +11,9 @@ print("Sensor Data App")
 
 # Module 1 code here:
 
+data = load_sensor_data()
+
+print(f"Loaded records: {len(data)}")
 # Module 2 code here:
 
 # Module 3 code here:
