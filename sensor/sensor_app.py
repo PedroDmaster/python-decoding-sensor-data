2bec2b7b534b151e8e804e3ed3b52b6364ada1f4
pedro.dg <pedro.goncalves.arch@gmail.com>
2022-10-03 16:58:42 +0100

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,7 +1,8 @@
 # Runner script for all modules
 
+from datetime import date, datetime
 from load_data import load_sensor_data
-
+from house_info import HouseInfo
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -14,8 +15,20 @@ print("Sensor Data App")
 data = load_sensor_data()
 
 print(f"Loaded records: {len(data)}")
+
 # Module 2 code here:
 
+house_info = HouseInfo(data)
+test_area = 1
+test_date = datetime.strftime('5/9/20','%m/%d/%y')
+
+recs = house_info.get_data_by_area('id', rec_area=test_area)
+
+print(f"\nHouse sensor records for area {test_area} = {len(recs)}")
+
+recs = house_info.get_data_by_date('id', rec_area=test_date)
+
+print(f"\nHouse sensor records for date {test_date.strftime('%m/%d/%y')} = {len(recs)}")
 # Module 3 code here:
 
 # Module 4 code here:
