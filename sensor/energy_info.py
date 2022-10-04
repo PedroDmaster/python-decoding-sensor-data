53a57d17aa54a66ea5c8da5698a3f1f7f6054514
pedro.dg <pedro.goncalves.arch@gmail.com>
2022-10-04 10:54:52 +0100

    Update energy_info.py

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -2,8 +2,8 @@ from house_info import HouseInfo
 from datetime import date, datetime
 
 class EnergyData(HouseInfo):
-    ENERGY_PER_BULB = float(0.2)
-    ENERGY_BITS = "0x0F0"
+    ENERGY_PER_BULB = 0.2
+    ENERGY_BITS = 0x0F0
     
     def _get_energy(self, rec):
         energy = int(rec, base=16)
